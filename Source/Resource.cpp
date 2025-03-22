06f8b749a515e4bc544f25b61e467333f4d7a1c6
mZake <zakeplaybr@gmail.com>
2025-03-21 23:07:44 -0300

    Put `ResourceManager` class in `Sol` namespace

--- Source/Resource.cpp
+++ Source/Resource.cpp
@@ -1,71 +1,73 @@
 #include "Resource.h"
 
-template <typename... Args>
-bool FileHasExtension(const std::filesystem::path& path, const Args&... args)
+namespace Sol
 {
-    return ((path.extension() == args) || ...);
-}
-
-ResourceManager::ResourceManager(uint32_t fontSize) : mFontSize(fontSize)
-{
-}
-
-void ResourceManager::LoadResources(const std::filesystem::path& directoryPath, SDL_Renderer* renderer)
-{
-    mTextures.reserve(16);
-    mFonts.reserve(16);
-
-    std::filesystem::recursive_directory_iterator entryList(directoryPath);
-    for (const auto& entry : entryList)
+    template <typename... Args>
+    bool FileHasExtension(const std::filesystem::path& path, const Args&... args)
     {
-        if (!entry.is_regular_file())
-            continue;
+        return ((path.extension() == args) || ...);
+    }
 
-        std::filesystem::path path = entry.path();
-        std::string pathString = path.string();
-        std::string assetId = path.filename().string();
-        SDL_Log("Asset ID: %s\n", assetId.c_str());
+    ResourceManager::ResourceManager(uint32_t fontSize) : mFontSize(fontSize)
+    {
+    }
 
-        if (FileHasExtension(path, ".png", ".jpg", ".jpeg", ".bmp"))
+    void ResourceManager::LoadResources(const std::filesystem::path& directoryPath, SDL_Renderer* renderer)
+    {
+        mTextures.reserve(16);
+        mFonts.reserve(16);
+
+        std::filesystem::recursive_directory_iterator entryList(directoryPath);
+        for (const auto& entry : entryList)
         {
-            SDL_Texture* texture = IMG_LoadTexture(renderer, pathString.c_str());
-            if (texture)
-                mTextures.emplace(assetId, texture);
-        }
-        else if (FileHasExtension(path, ".ttf", ".otf"))
-        {
-            TTF_Font* font = TTF_OpenFont(pathString.c_str(), mFontSize);
-            if (font)
-                mFonts.emplace(assetId, font);
+            if (!entry.is_regular_file())
+                continue;
+
+            std::filesystem::path path = entry.path();
+            std::string pathString = path.string();
+            std::string assetId = path.filename().string();
+
+            if (FileHasExtension(path, ".png", ".jpg", ".jpeg", ".bmp"))
+            {
+                SDL_Texture* texture = IMG_LoadTexture(renderer, pathString.c_str());
+                if (texture)
+                    mTextures.emplace(assetId, texture);
+            }
+            else if (FileHasExtension(path, ".ttf", ".otf"))
+            {
+                TTF_Font* font = TTF_OpenFont(pathString.c_str(), mFontSize);
+                if (font)
+                    mFonts.emplace(assetId, font);
+            }
         }
     }
-}
 
-void ResourceManager::UnloadResources()
-{
-    for (auto& [_, texture] : mTextures)
-        SDL_DestroyTexture(texture);
-    for (auto& [_, font] : mFonts)
-        TTF_CloseFont(font);
+    void ResourceManager::UnloadResources()
+    {
+        for (auto& [_, texture] : mTextures)
+            SDL_DestroyTexture(texture);
+        for (auto& [_, font] : mFonts)
+            TTF_CloseFont(font);
 
-    mTextures.clear();
-    mFonts.clear();
-}
+        mTextures.clear();
+        mFonts.clear();
+    }
 
-SDL_Texture* ResourceManager::GetTexture(const std::string& id) const
-{
-    auto item = mTextures.find(id);
-    if (item == mTextures.end())
-        return nullptr;
+    SDL_Texture* ResourceManager::GetTexture(const std::string& id) const
+    {
+        auto item = mTextures.find(id);
+        if (item == mTextures.end())
+            return nullptr;
 
-    return item->second;
-}
+        return item->second;
+    }
 
-TTF_Font* ResourceManager::GetFont(const std::string& id) const
-{
-    auto item = mFonts.find(id);
-    if (item == mFonts.end())
-        return nullptr;
+    TTF_Font* ResourceManager::GetFont(const std::string& id) const
+    {
+        auto item = mFonts.find(id);
+        if (item == mFonts.end())
+            return nullptr;
 
-    return item->second;
+        return item->second;
+    }
 }
